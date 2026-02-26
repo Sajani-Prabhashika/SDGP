4890bca0f0dbd725d60fe22ebd6cb1e132e15759
Manuga <manuga.006@gmail.com>
2026-02-26 20:35:09 +0530

    Modified the comment

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.prediction import prediction_bp
 
-#Setup the app
+#Setup the Flask app (Backend)
 def init_app():
     app = Flask(__name__)
     CORS(app)
